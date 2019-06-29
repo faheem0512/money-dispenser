c3a698979ee76b864b00c55b250363213145663f
Faheem Ahmad <faheem.ahmad@u2opiamobile.com>
2019-06-29 20:51:29 +0530

    Updates

---
+++ precache-manifest.06f5b8d4c7c1b92f9640de75733795e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5385f22c2d054cf927d1088e9d12d29",
+    "url": "/money-dispenser/index.html"
+  },
+  {
+    "revision": "b691853694d6537dfea2",
+    "url": "/money-dispenser/static/css/main.25834e05.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/money-dispenser/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "b691853694d6537dfea2",
+    "url": "/money-dispenser/static/js/main.f6fedc61.chunk.js"
+  },
+  {
+    "revision": "6ce09876888b277c4efb",
+    "url": "/money-dispenser/static/js/runtime~main.553a211c.js"
+  }
+]);
